7896ee09609eedcfb41797ae7006377d2ae59f31
Daniel Alejandro Gutierrez <dagmcisco@gmail.com>
2018-10-08 19:51:13 -0400

    creacion de componentes

---
+++ loginAPP/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { RegistroPageComponent } from './components/registro-page/registro-page.component';
+import { LoginPageComponent } from './components/login-page/login-page.component';
+import { PrivadoPageComponent } from './components/privado-page/privado-page.component';
+import { NotFoundPageComponent } from './components/not-found-page/not-found-page.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomePageComponent,
+    NavbarComponent,
+    RegistroPageComponent,
+    LoginPageComponent,
+    PrivadoPageComponent,
+    NotFoundPageComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
