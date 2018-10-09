ce4c1f09e6fd10b8f66820b8c18cce7e0928c5e6
Daniel Alejandro Gutierrez <dagmcisco@gmail.com>
2018-10-08 22:33:24 -0400

    navbar y rutas

--- loginAPP/src/app/app-routing.module.ts
+++ loginAPP/src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import {HomePageComponent} from './components/home-page/home-page.component';
+import {LoginPageComponent} from './components/login-page/login-page.component';
+import {RegistroPageComponent} from './components/registro-page/registro-page.component';
+import {PrivadoPageComponent} from './components/privado-page/privado-page.component';
+import {NotFoundPageComponent} from './components/not-found-page/not-found-page.component';
+
+
+
+
+const routes: Routes = [
+  { path :'', component: HomePageComponent},
+  { path :'login', component: LoginPageComponent},
+  { path :'register', component: RegistroPageComponent},
+  { path :'privado', component: PrivadoPageComponent},
+  { path :'**', component: NotFoundPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
