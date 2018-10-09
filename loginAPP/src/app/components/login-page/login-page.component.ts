d0197e0b5da8b3867d25f6e5cd2c5953185da77d
Daniel Alejandro Gutierrez <dagmcisco@gmail.com>
2018-10-09 09:10:50 -0400

    maquetacion loginPage RegistroPage NotFoundPage

--- loginAPP/src/app/components/login-page/login-page.component.ts
+++ loginAPP/src/app/components/login-page/login-page.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 
+import {Router } from '@angular/router';
+
+
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.scss']
 })
 export class LoginPageComponent implements OnInit {
-
-  constructor() { }
+  
+  constructor( ) { }
 
   ngOnInit() {
+  }
+
+  onSubmitLogin() {
+    
+  }
+
+  onClickGoogleLogin() {
+  }
+
+  onClickFacebookLogin() {
+  }
+
+  onClickTwitterLogin() {
   }
 
 }
